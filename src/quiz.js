e6991634aa3be59c119050d9ad3223252dcfc8f6
Gianfranco <gianfranco.devita@hotmail.com>
2026-02-23 17:13:38 +0100

    Day 1 Solved

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,41 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+	constructor(questions, timeLimit, timeRemaining) {
+		this.questions = questions;
+		this.timeLimit = timeLimit;
+		this.timeRemaining = timeRemaining;
+		this.correctAnswers = 0;
+		this.currentQuestionIndex = 0;
+	}
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+	getQuestion() {
+		return this.questions[this.currentQuestionIndex];
+	}
 
-    // 4. shuffleQuestions()
+	moveToNextQuestion() {
+		this.currentQuestionIndex++;
+	}
 
-    // 5. checkAnswer(answer)
+	shuffleQuestions() {
+		for (let i = this.questions.length - 1; i > 0; i--) {
+			const randomIndex = Math.floor(Math.random() * (i + 1));
+			const temp = this.questions[i];
+			this.questions[i] = this.questions[randomIndex];
+			this.questions[randomIndex] = temp;
+		}
+	}
 
-    // 6. hasEnded()
+	checkAnswer(answer) {
+		let currentQuestion = this.getQuestion();
+		if (answer === currentQuestion.answer) {
+			this.correctAnswers++;
+		}
+	}
+
+	hasEnded() {
+		if (this.currentQuestionIndex < this.questions.length) {
+			return false;
+		} else if (this.currentQuestionIndex === this.questions.length) {
+			return true;
+		}
+	}
 }
